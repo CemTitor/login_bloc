79b1969753a7666d3e66867d3b01b9f2d9e33ac5
CemYilmaz <cemyilmaz463@gmail.com>
2022-11-30 18:26:08 +0300

    Username and password validation switched

--- lib/login/models/password.dart
+++ lib/login/models/password.dart
@@ -1,6 +1,12 @@
 import 'package:formz/formz.dart';
 
-enum PasswordValidationError { empty }
+enum PasswordValidationError {
+  empty,
+  notLongEnough,
+  notSpecialCharacters,
+  notNumbers,
+  notLetters,
+}
 
 class Password extends FormzInput<String, PasswordValidationError> {
   const Password.pure() : super.pure('');
@@ -8,7 +14,17 @@ class Password extends FormzInput<String, PasswordValidationError> {
 
   @override
   PasswordValidationError? validator(String value) {
-    if (value.isEmpty) return PasswordValidationError.empty;
+    if (value.isEmpty) {
+      return PasswordValidationError.empty;
+    } else if (value.length < 8) {
+      return PasswordValidationError.notLongEnough;
+    } else if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return PasswordValidationError.notSpecialCharacters;
+    } else if (!value.contains(RegExp(r'[0-9]'))) {
+      return PasswordValidationError.notNumbers;
+    } else if (!value.contains(RegExp(r'[a-zA-Z]'))) {
+      return PasswordValidationError.notLetters;
+    }
     return null;
   }
 }
--- lib/login/models/username.dart
+++ lib/login/models/username.dart
@@ -2,10 +2,6 @@ import 'package:formz/formz.dart';
 
 enum UsernameValidationError {
   empty,
-  notLongEnough,
-  notSpecialCharacters,
-  notNumbers,
-  notLetters
 }
 
 class Username extends FormzInput<String, UsernameValidationError> {
@@ -14,17 +10,7 @@ class Username extends FormzInput<String, UsernameValidationError> {
 
   @override
   UsernameValidationError? validator(String value) {
-    if (value.isEmpty)
-      return UsernameValidationError.empty;
-    else if (value.length < 3)
-      return UsernameValidationError.notLongEnough;
-    else if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value))
-      return UsernameValidationError.notSpecialCharacters;
-    else if (!RegExp(r'[0-9]').hasMatch(value))
-      return UsernameValidationError.notNumbers;
-    else if (!RegExp(r'[a-zA-Z]').hasMatch(value))
-      return UsernameValidationError.notLetters;
-    else
-      return null;
+    if (value.isEmpty) return UsernameValidationError.empty;
+    return null;
   }
 }
